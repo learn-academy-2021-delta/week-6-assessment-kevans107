eaae43bbaa956770aa1de896630de26a0fa0e6cd
kevans107 <kellyevans107.gmail.com>
2021-10-24 23:42:47 -0400

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,10 +24,24 @@ var people = [
 ]
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe("eachPerson", () => {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () =>{
+    expect(eachPerson(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
 
 // b) Create the function that makes the test pass.
 
+
+const eachPerson = (array) => {
+  // Create a function called eachPerson that takes in an array of objects
+  return array.map(value => {
+    // Use .map to iterate through each value
+    return `${value.name.replace(/(?:^|\s)\S/g, function(finalName) {return finalName.toUpperCase()})} is ${value.occupation}.`
+      // use string interpolation to return an array with a sentence about each person. Use .replace with a regex function on value.name to return the first letter of each string capitalized.
+    })
+  }
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -40,10 +54,32 @@ var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
 
+describe("divThree", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(divThree(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(divThree(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
 
 // b) Create the function that makes the test pass.
 
 
+  const divThree = (array) => {
+    // create a function called divThree that takes in an array
+    let finalNum = []
+    // declare a variable called finalNum to push all number data types to
+    for(let i = 0; i < array.length; i++){
+      // use a for loop to iterate through the array
+      if(typeof array[i] === "number"){
+        // if the data type at the index is a number push the number to the finalNum array and return the remainder of the value % 3
+        finalNum.push(array[i] % 3)
+      }
+    }
+    return finalNum
+    // return the array with only the numbers % 3
+  }
+  
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -54,6 +90,24 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
-
+describe("cubeSum", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(cubeSum([2, 3, 4])).toEqual(99)
+    expect(cubeSum([0, 5, 10])).toEqual(1125)
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+const cubeSum = (array) => {
+  // create a variable called cubeSum that takes in an array as a parameter
+  let finalSum = 0
+  // create a new variable called finalSum that is set to zero to store the results of the for loop and cube calculation
+  for(let i = 0; i < array.length; i++){
+    finalSum = finalSum + array[i] ** 3
+    // create a for loop that cubes each value and adds the resulting values to each other within the finalSum variable
+  }
+  return finalSum
+  // return the total in finalSum
+}
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -9,22 +9,22 @@
 # ---1)
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) The controller method index is returning all instances of BlogPosts within the database
     @posts = BlogPost.all
   end
 
   def show
-    # ---3)
+    # ---3) The controller method show is using the .find method to return a specific id within the URL. The line is enumerating through the database to find a specific id within the model.
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) The controller method new is use to create a form for the user to input new information to the database 
   def new
     @post = Post.new
   end
 
   def create
-    # ---5)
+    # ---5) The controller method create is the second piece after the form is created with the 'new' controller method. The create method is generating the new user input into the database with specific parameters to control what is input by users. The method 'blog_post_params' are passed through the .create() built-in method to tell the application what information is required and what is permitted. A conditional statement tells the program to redirect the user to the new inputted blog post after it is submitted, or if the new post is not valid it will keep the user on the same page with the redirect to new_blog_post_path. The 'blog_post' and 'new_blog_post' on line 30 and 32 reference a specific route, and _path is how the program understands that is the location the user needs to be redirected to. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,14 +33,14 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  # ---6)
+  # ---6) The controller method edit is using the .find built-in method to identify a specific blog post instance to be edited within the database. An input from the user selecting a specific instance will allow the database instance to be edited by the user. 
   def edit
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) The controller update method is taking in the user inputted information and comparing it against the blog_post_params which require a :blog_post entry and permits the user to include a :title and :content. A conditional statement indicates that if the post meets the params then the user will be redirected to the blog_post_path they have just created. Otherwise, users will remain on the edit_blog_post page.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) The redirect_to blog_post_path(@post) will take the user to the blog_post page they were attempting to delete if the application was unable to destry the database instance. 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) Private means that everything below this line in the controller file is inexcessible to the user. This is a security measure so that hackers and others are unable to alter the params within the application. This helps to keep the application and database organized and secure. 
   private
   def blog_post_params
-    # ---10)
+    # ---10) Below are the required and permitted params for the controller method blog_post_params. The code indicates which user inpuuted information is required (:blog_post) and which are permitted for the user (:title, :content)
     params.require(:blog_post).permit(:title, :content)
   end
 end
